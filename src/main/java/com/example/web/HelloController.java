e8acb28a530beb7a39d422be88e36a10fc685f4b
flymanflyman <406544970@qq.com>
2018-09-29 00:41:35 +0800

    20180929 from home

--- src/main/java/com/example/web/HelloController.java
+++ src/main/java/com/example/web/HelloController.java
@@ -3,14 +3,34 @@ package com.example.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ */
 @RestController
 public class HelloController {
+    /**
+     * @return
+     */
     @RequestMapping("/hello")
     public String index(){
         return "hello world";
     }
+
+    /**
+     * @return
+     */
+    @RequestMapping("/hello3")
+    public boolean isGet(){
+        return true;
+    }
+
+    /**
+     * @param index
+     * @return
+     */
     @RequestMapping("/hello2")
     public String hello2(int index){
+
         Integer myIndex = new Integer(index);
         if (myIndex == null)
              index = 0;
@@ -18,5 +38,6 @@ public class HelloController {
         counter ++;
         counter += index;
         return String.format("Counter:[%d]",counter);
+
     }
 }
