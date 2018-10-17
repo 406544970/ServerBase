2ba5d99f27e4aca347cdae5a66fadeea871dab6d
flymanflyman <406544970@qq.com>
2018-10-17 23:24:07 +0800

    20181017from home

--- src/main/java/com/example/web/HelloController.java
+++ src/main/java/com/example/web/HelloController.java
@@ -1,10 +1,14 @@
 package com.example.web;
 
+import com.netflix.discovery.DiscoveryManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  *
@@ -15,11 +19,21 @@ public class HelloController {
         super();
         this.version = "0.0.1";
     }
+    @Autowired
+    private DiscoveryClient client;
 
+    @RequestMapping(value = "/upLine", method = RequestMethod.GET)
+    public String index() {
+        java.util.List<ServiceInstance> instances = client.getInstances("hello-service");
+        return "Hello World";
+    }
     private String version;
 
     @Value("${server.port}")
     private String port;
+
+    @Value("${spring.application.name}")
+    private String springApplicationName;
 
     /**
      * @return 返回当前版本号
@@ -32,8 +46,8 @@ public class HelloController {
     /**
      * @return 返回当前端口号
      */
-    @RequestMapping("/myHello")
-    public String myHello(){
+    @RequestMapping("/myPort")
+    public String myPort(){
         return String.format("I am is port:%s",port);
     }
 
@@ -61,4 +75,10 @@ public class HelloController {
         return String.format("Counter:[%d]",counter);
 
     }
+    @RequestMapping(value = "/downLine",method = RequestMethod.GET)
+    public String downLine(){
+        DiscoveryManager.getInstance().shutdownComponent();
+        return String.format("ApplicationName\"%s\"(Port:%s) is downLine.",this.springApplicationName,this.port);
+    }
+
 }
